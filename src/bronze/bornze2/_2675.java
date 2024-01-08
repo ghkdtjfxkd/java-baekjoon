93737ddb146b22bb153a6dbbe11dc43ea349766c
ghkdtjfxkd <syt1128@gmail.com>
2024-01-08 14:31:01 +0900

    :tada: Sol _2675
    문자열 반복 bronze2

---
+++ src/bronze/bornze2/_2675.java
@@ -0,0 +1,35 @@
+package bronze.bornze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2675 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count =Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+            String input = br.readLine();
+            String[] inputArr = input.split(" ");
+
+            int repeats = Integer.parseInt(inputArr[0]);
+
+            String repeatStr = inputArr[1];
+            String[] repeatStrSplit = repeatStr.split("");
+
+            String p = "a";
+            StringBuilder stringBuilder = new StringBuilder(p);
+
+            for (String s : repeatStrSplit) {
+                for (int j = 0; j < repeats; j++) {
+                    stringBuilder.append(s);
+                }
+            }
+            stringBuilder.delete(0, 1);
+            System.out.println(stringBuilder);
+        }
+    }
+}
