ee175e02a5bee2e02bb679a6b48677c4dc9727c4
ghkdtjfxkd <syt1128@gmail.com>
2024-01-08 13:51:16 +0900

    :tada: Sol _10809
    알파벳 찾기

---
+++ src/bronze/bornze2/_10809.java
@@ -0,0 +1,25 @@
+package bronze.bornze2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _10809 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+
+        List<String> token = new ArrayList<>(List.of(s.split("")));
+        List<String> alphabet = new ArrayList<>(List.of("abcdefghijklmnopqrstuvwxyz".split("")));
+
+        alphabet.stream().map(token::indexOf).forEach(x -> System.out.print(x + " "));
+    }
+
+}
