eb281fd32f53a5a987e5d8891e8be18f6631a643
ghkdtjfxkd <syt1128@gmail.com>
2024-01-08 14:08:12 +0900

    :tada: Sol _8958
    OX퀴즈

---
+++ src/bronze/bornze2/_8958.java
@@ -0,0 +1,44 @@
+package bronze.bornze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _8958 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+
+            int total = 0;
+            String OXCase = br.readLine();
+
+            List<String> splitForX = new ArrayList<>(List.of(OXCase.split("X")));
+
+            for (String Os: splitForX) {
+              total += cals(Os.length());
+            }
+            System.out.println(total);
+        }
+
+
+
+
+
+    }
+
+    static int cals (int count){
+        if(count == 0){
+            return 0;
+        }
+        else{
+            return count + cals(count -1);
+        }
+    }
+}
