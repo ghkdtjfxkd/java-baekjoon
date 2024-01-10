87984e4835cd2ad3e9d9382b6d279ecd037d448b
ghkdtjfxkd <syt1128@gmail.com>
2024-01-10 11:13:46 +0900

    :tada: Sol _2750
    수 정렬하기 bronze2

---
+++ src/bronze/bornze2/_2750.java
@@ -0,0 +1,25 @@
+package bronze.bornze2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        List<Integer> numList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            int input = Integer.parseInt(br.readLine());
+            numList.add(input);
+        }
+       numList.stream().sorted().forEach(System.out::println);
+    }
+}
