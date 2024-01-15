9de5b5e8a287e68030f1df86d66b959a8f94f728
ghkdtjfxkd <syt1128@gmail.com>
2024-01-15 13:33:55 +0900

    :tada: Sol _2440
    별 찍기 - 3 bronze4

---
+++ src/bronze/bronze4/_2440.java
@@ -0,0 +1,20 @@
+package bronze.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = count; i > 0 ; i--) {
+            for (int j = 0; j < i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
