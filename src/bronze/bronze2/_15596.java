5a79e4758061e15c1d82341481f2da6ebf356210
ghkdtjfxkd <syt1128@gmail.com>
2024-01-15 12:34:20 +0900

    :tada: Sol _15596
    정수 N개의 합 bronze2

---
+++ src/bronze/bronze2/_15596.java
@@ -0,0 +1,16 @@
+package bronze.bronze2;
+
+public class _15596 {
+    public static void main(String[] args) {
+    }
+}
+
+class Test {
+    long sum (int[] a){
+        long n  = 0;
+        for (int i = 0; i < a.length; i++) {
+            n += a[i];
+        }
+        return n;
+    }
+}
