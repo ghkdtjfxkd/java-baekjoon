5680cd2cd4ccdf4cd9f94fb23e1365059fcace90
ghkdtjfxkd <syt1128@gmail.com>
2024-01-30 21:49:19 +0900

    :tada: Sol _25083
    새싹 bronze5

---
+++ src/bronze/bronze5/_25083.java
@@ -0,0 +1,18 @@
+package bronze.bronze5;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class _25083 {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |");
+        bw.flush();
+    }
+}
