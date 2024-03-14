2e70b61d8d39072d0f9f1717ee16deedbd387165
ghkdtjfxkd <syt1128@gmail.com>
2024-03-14 11:39:38 +0900

    :feat: Sol _15894
    수학은 체육과목입니다. bronze3

---
+++ src/bronze/bronze3/_15894.java
@@ -0,0 +1,13 @@
+package bronze.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15894 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        long n = Long.parseLong(br.readLine());
+        System.out.println(4 * n);
+    }
+}
