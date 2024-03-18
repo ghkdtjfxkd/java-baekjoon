05539533be7fb08512eaffc85df693f164872599
ghkdtjfxkd <syt1128@gmail.com>
2024-03-18 13:54:58 +0900

    :feat: Sol _25350
     커트라인 bronze2

---
+++ src/bronze/bronze2/_25305.java
@@ -0,0 +1,26 @@
+package bronze.bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _25305 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] scores = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores);
+        System.out.println(scores[n-k]);
+    }
+}
