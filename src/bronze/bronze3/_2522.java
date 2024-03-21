09d072374b290455a3f427453fe5e97a085603b9
ghkdtjfxkd <syt1128@gmail.com>
2024-03-21 10:18:43 +0900

    :feat: Sol _2522
    별 찍기 - 12 bronze3

---
+++ src/bronze/bronze3/_2522.java
@@ -0,0 +1,35 @@
+package bronze.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                sb.append(" ");
+            }
+            for (int j = n - i - 1; j < n; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(" ");
+            }
+            for (int j = i + 1; j < n; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
