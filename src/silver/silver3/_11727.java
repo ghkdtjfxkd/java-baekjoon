7e7fe9ab6f9937b65b2c93e2b6dd5d9921f66283
ghkdtjfxkd <syt1128@gmail.com>
2024-03-21 12:36:18 +0900

    :feat: Sol _11727
    2xn 타일링 2 silver3

---
+++ src/silver/silver3/_11727.java
@@ -0,0 +1,28 @@
+package silver.silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11727 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static Integer dp[];
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        dp = new Integer[n + 1];
+        System.out.println(recur(n));
+    }
+
+    private static int recur(int n){
+        if(dp[n] == null){
+            if(n == 0){
+                dp[n] =1;
+            } else if(n == 1){
+                dp[n] =1;
+            } else {
+                dp[n] = recur(n-1) + recur(n-2) + recur(n-2);
+            }
+        }
+        return dp[n] %10007;
+    }
+}
