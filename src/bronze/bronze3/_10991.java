01aa6ce5b87c9035db8e879295382f51ca880ff5
ghkdtjfxkd <syt1128@gmail.com>
2024-03-21 10:47:04 +0900

    :feat: Sol _10991
    별 찍기 - 16 bronze3

---
+++ src/bronze/bronze3/_10991.java
@@ -0,0 +1,24 @@
+package bronze.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10991 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n - 1 - i; j++) {
+                    sb.append(" ");
+                }
+                sb.append("*");
+                for (int j = 0; j < i; j++) {
+                    sb.append(" ").append("*");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+    }
+}
